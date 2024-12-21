31da95aa80e7ab1e18adab5720f0d275b9743ccd
Bopleromn <bopleromn@gmail.com>
2024-12-21 16:01:54 +0300

    #FIX connection to db

--- api/database.py
+++ api/database.py
@@ -3,7 +3,7 @@ from prisma import Prisma
 from contextlib import asynccontextmanager
 
 from api.logger import get_logger
-
+from api.main import app
 
 prisma = Prisma()
 logger = get_logger('Database')
@@ -11,12 +11,16 @@ logger = get_logger('Database')
 
 @asynccontextmanager
 async def db_session() -> AsyncGenerator[Prisma, None]:
-    if not prisma.is_connected():
-        logger.info('Connecting to database...')
-        await prisma.connect()
-    try:
-        yield prisma
-    except Exception as e:
-        logger.error(e)
-    finally:
-        await prisma.disconnect()
+    yield prisma
+
+
+@app.on_event("startup")
+async def on_startup():
+    logger.info("Подключение к базе данных...")
+    await prisma.connect()
+
+@app.on_event("shutdown")
+async def on_shutdown():
+    logger.info("Отключение от базы данных...")
+    await prisma.disconnect()
+    
